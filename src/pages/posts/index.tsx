fcdd1d522b29eb8d9a346afb76e035f73a404611
Rafael Martins <rafaeldcmartins@gmail.com>
2021-03-30 17:00:13 -0300

    CMS - consume Prismic API

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,4 +1,7 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head'
+import Prismic from '@prismicio/client';
+import { getPrismicClient } from '../../services/prismic';
 
 import styles from './styles.module.scss';
 
@@ -44,3 +47,23 @@ const Posts = () => {
 };
 
 export default Posts;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query([
+    Prismic.predicates.at('document.type', 'post')
+  ], {
+    fetch: ['post.title', 'post.content'],
+    pageSize: 100,
+  });
+
+  console.log(JSON.stringify(response, null, 2));
+
+
+  return {
+    props: {
+
+    }
+  }
+};
