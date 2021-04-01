fb2ae3080f737ef3d36f67430e69a209a6a92514
Rafael Martins <rafaeldcmartins@gmail.com>
2021-04-01 12:47:32 -0300

    Static Pages - generate static previews

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -28,7 +28,7 @@ const Posts = ({ posts }: PostsProps) => {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map(post => (
-            <Link key={post.slug} href={`/posts/${post.slug}`}>
+            <Link key={post.slug} href={`/posts/preview/${post.slug}`}>
               <a>
                 <time>{post.updatedAt}</time>
                 <strong>{post.title}</strong>
--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import Head from "next/head";
 import Link from "next/link";
-import { GetStaticProps } from "next";
+import { GetStaticPaths, GetStaticProps } from "next";
 import { useRouter } from "next/router";
 import { useSession } from "next-auth/client";
 import { RichText } from "prismic-dom";
@@ -60,7 +60,7 @@ const PostPreview = ({ post }: PostPreviewProps) => {
 
 export default PostPreview;
 
-export const getStaticPaths = () => {
+export const getStaticPaths: GetStaticPaths = async () => {
   return {
     paths: [],
     fallback: 'blocking',
@@ -86,6 +86,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   return {
     props: {
       post
-    }
+    },
+    revalidate: 60 * 30 // 30 minutes
   }
 }
