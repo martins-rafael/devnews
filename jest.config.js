71dcf30704b8f68eb6cba4f017092b4385ed8378
Rafael Martins <rafaeldcmartins@gmail.com>
2021-07-17 11:24:14 -0300

    Getting Started with Testing - Jest and RTL config

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  setupFilesAfterEnv: ['<rootDir>/src/tests/setupTests.ts'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
+  },
+};
