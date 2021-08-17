1995e397d1742a7d52dd6e1fa7662d28c2b7ed62
Rafael Martins <rafaeldcmartins@gmail.com>
2021-08-17 20:44:40 -0300

    Testing Pages - testing getStaticProps

--- src/tests/pages/Home.spec.tsx
+++ src/tests/pages/Home.spec.tsx
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react';
-import Home from '../../pages';
+import { mocked } from 'ts-jest/utils';
+import { stripe } from '../../services/stripe';
+import Home, { getStaticProps } from '../../pages';
 
 jest.mock('next/router');
 jest.mock('next-auth/client', () => {
@@ -7,11 +9,34 @@ jest.mock('next-auth/client', () => {
     useSession: () => [null, false],
   };
 });
+jest.mock('../../services/stripe');
 
 describe('Home page', () => {
   it('should render correctly', () => {
     render(<Home product={{ priceId: 'fake-price-id', amount: 'R$10,00' }} />);
 
     expect(screen.getByText('por R$10,00/mês')).toBeInTheDocument();
+  });
+
+  it('should load initial data', async () => {
+    const retrieveStripePricesMocked = mocked(stripe.prices.retrieve);
+
+    retrieveStripePricesMocked.mockResolvedValueOnce({
+      id: 'fake-price-id',
+      unit_amount: 1000,
+    } as any);
+
+    const response = await getStaticProps({});
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          product: {
+            priceId: 'fake-price-id',
+            amount: '$10.00'
+          }
+        }
+      })
+    )
   });
 });
